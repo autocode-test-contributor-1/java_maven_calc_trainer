dbfd50b5c695637e65eb023e4b961e612706a551
Alexander Oblizin <Alexander_Oblizin@epam.com>
2021-06-29 14:25:55 +0300

    QualityGateIssuesAndFailed test

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -4,7 +4,7 @@ package com.epam;
 public class CalcServiceImpl implements CalcService {
     @Override
     public int add(int a, int b) {
-//        var c = a + b;
+        var c = a + b;
         return a + b;
     }
 
